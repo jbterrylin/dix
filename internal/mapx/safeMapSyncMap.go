a0361afdd5bd14b2af2e8c07c4797dcbae9df92c
秋及北 <wanglinlee1707@gmail.com>
2025-07-13 15:40:09 +0800

    replace SafeMap using sync.Map

--- internal/mapx/safeMapSyncMap.go
+++ internal/mapx/safeMapSyncMap.go
@@ -1,95 +1,97 @@
 package mapx
 
-// type ISafeMapKey interface {
-// }
+import "sync"
 
-// // SafeMap provides a map alternative to avoid memory leak.
-// // This implementation is not needed until issue below fixed.
-// // https://github.com/golang/go/issues/20135
+type ISafeMapKey interface {
+}
 
-// var defaultMaxDeleteCount int64 = 1000
+// SafeMap provides a map alternative to avoid memory leak.
+// This implementation is not needed until issue below fixed.
+// https://github.com/golang/go/issues/20135
 
-// func SetDefaultMaxDeleteCount(newDefaultMaxDeleteCount int64) {
-// 	defaultMaxDeleteCount = newDefaultMaxDeleteCount
-// }
+var defaultMaxDeleteCount int64 = 1000
 
-// type SafeMap[Key comparable, Value any] struct {
-// 	maxDeleteCount int64
-// 	m              *sync.Map
-// 	deleteCount    int64
-// }
+func SetDefaultMaxDeleteCount(newDefaultMaxDeleteCount int64) {
+	defaultMaxDeleteCount = newDefaultMaxDeleteCount
+}
 
-// func NewSafeMap[Key comparable, Value any](opts ...SafeMapOption) *SafeMap[Key, Value] {
-// 	// handle options
-// 	var opt safeMapOption
-// 	for _, o := range opts {
-// 		o(&opt)
-// 	}
+type SafeMap[Key comparable, Value any] struct {
+	maxDeleteCount int64
+	m              *sync.Map
+	deleteCount    int64
+}
 
-// 	maxDeleteCount := opt.maxDeleteCount
-// 	if maxDeleteCount == 0 {
-// 		maxDeleteCount = defaultMaxDeleteCount
-// 	}
+func NewSafeMap[Key comparable, Value any](opts ...SafeMapOption) *SafeMap[Key, Value] {
+	// handle options
+	var opt safeMapOption
+	for _, o := range opts {
+		o(&opt)
+	}
 
-// 	return &SafeMap[Key, Value]{
-// 		maxDeleteCount: maxDeleteCount,
-// 		m:              new(sync.Map),
-// 	}
-// }
+	maxDeleteCount := opt.maxDeleteCount
+	if maxDeleteCount == 0 {
+		maxDeleteCount = defaultMaxDeleteCount
+	}
 
-// // Set 插入或更新
-// func (s *SafeMap[Key, Value]) Set(key Key, val Value) {
-// 	s.m.Store(key, val)
-// }
+	return &SafeMap[Key, Value]{
+		maxDeleteCount: maxDeleteCount,
+		m:              new(sync.Map),
+	}
+}
 
-// // Get 读取
-// func (s *SafeMap[Key, Value]) Get(key Key) (Value, bool) {
-// 	v, ok := s.m.Load(key)
-// 	if !ok {
-// 		var zero Value
-// 		return zero, ok
-// 	}
-// 	return v.(Value), ok
-// }
+// Set 插入或更新
+func (s *SafeMap[Key, Value]) Set(key Key, val Value) {
+	s.m.Store(key, val)
+}
 
-// // Del 删除，并在阈值时重建底层 map
-// func (s *SafeMap[Key, Value]) Del(key Key) {
-// 	// 删除并计数
-// 	if _, ok := s.m.Load(key); ok {
-// 		s.m.Delete(key)
-// 		s.deleteCount++
-// 	}
-// 	// 超过阈值，做一次 shrink
-// 	if s.deleteCount >= s.maxDeleteCount {
-// 		var newMap sync.Map
-// 		s.m.Range(func(key, value any) bool {
-// 			newMap.Store(key, value)
-// 			return true
-// 		})
-// 		s.m = &newMap
-// 		s.deleteCount = 0
-// 	}
-// }
+// Get 读取
+func (s *SafeMap[Key, Value]) Get(key Key) (Value, bool) {
+	v, ok := s.m.Load(key)
+	if !ok {
+		var zero Value
+		return zero, ok
+	}
+	return v.(Value), ok
+}
 
-// // Size 返回当前元素数量
-// func (s *SafeMap[Key, Value]) Size() int {
-// 	n := 0
-// 	s.m.Range(func(_, _ any) bool {
-// 		n++
-// 		return true
-// 	})
-// 	return n
-// }
+// Del 删除，并在阈值时重建底层 map
+func (s *SafeMap[Key, Value]) Del(key Key) {
+	// 删除并计数
+	if _, ok := s.m.Load(key); ok {
+		s.m.Delete(key)
+		s.deleteCount++
+	}
+	// 超过阈值，做一次 shrink
+	if s.deleteCount >= s.maxDeleteCount {
+		var newMap sync.Map
+		s.m.Range(func(key, value any) bool {
+			newMap.Store(key, value)
+			return true
+		})
+		s.m = &newMap
+		s.deleteCount = 0
+	}
+}
 
-// // Range 迭代所有 kv
-// func (s *SafeMap[Key, Value]) Range(f func(key Key, val Value) bool) {
-// 	s.m.Range(func(key, value any) bool {
-// 		return f(key.(Key), value.(Value))
-// 	})
-// }
+// Size 返回当前元素数量
+func (s *SafeMap[Key, Value]) Size() int {
+	n := 0
+	s.m.Range(func(_, _ any) bool {
+		n++
+		return true
+	})
+	return n
+}
 
-// func (s *SafeMap[Key, Value]) GetOrSet(key Key, init func() Value) (val Value, alreadyInit bool) {
-// 	tmp, loaded := s.m.LoadOrStore(key, init())
-// 	val = tmp.(Value)
-// 	return val, loaded
-// }
+// Range 迭代所有 kv
+func (s *SafeMap[Key, Value]) Range(f func(key Key, val Value) bool) {
+	s.m.Range(func(key, value any) bool {
+		return f(key.(Key), value.(Value))
+	})
+}
+
+func (s *SafeMap[Key, Value]) GetOrSet(key Key, init func() Value) (val Value, alreadyInit bool) {
+	tmp, loaded := s.m.LoadOrStore(key, init())
+	val = tmp.(Value)
+	return val, loaded
+}
